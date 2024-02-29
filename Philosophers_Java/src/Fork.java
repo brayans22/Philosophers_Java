3fdc7c7706afc870083e200a60dd32f384b5bd93
Brayan Saiago <90729742+brayans22@users.noreply.github.com>
2024-02-29 02:00:49 +0100

    Delete Philosophers_Java directory

--- Philosophers_Java/src/Fork.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Fork {
-    private int id;
-    public Fork(int _id) { this.id = _id; }
-    public boolean areEquals(int _id) { return (this.id == _id); }
-
-}
